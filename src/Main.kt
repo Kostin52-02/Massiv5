ea0c4bbe6250770a9c62fe519c88e54fda420126
Kostin52-02 <157310420+Kostin52-02@users.noreply.github.com>
2024-10-29 11:14:34 +0300

    Update Main.kt

--- src/Main.kt
+++ src/Main.kt
@@ -1,14 +1,8 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+    val words = listOf("eat", "tea", "tan", "ate", "nat", "bat")
+    val groupedWords = words.groupBy { it.toCharArray().sorted().joinToString("") }
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+    for (group in groupedWords.values) {
+        println(group.joinToString(", "))
     }
 }
